774577c05f33af6c52353d168373d7a1144d772a
TommyWang <tommycx1999@gmail.com>
2023-04-26 23:52:55 -0400

    First commit

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ server.listen(port, () => {
 });
 const SERVER_ENDPOINT_REDIRECT = process.env.SERVER_ENDPOINT_REDIRECT || "localhost:3000";
 const CLIENT_KEY = "awahrdz5tzywmmd2";
+
 app.get("/login", (req, res) => {
     const csrfState = Math.random().toString(36).substring(2);
     res.cookie("csrfState", csrfState, { maxAge: 60000 });
@@ -30,3 +31,55 @@ app.get("/login", (req, res) => {
 
     res.redirect(url);
 });
+
+app.get('/show', (req, res) => {
+    const { code, state } = req.query;
+    const { csrfState } = req.cookies;
+
+    if (state !== csrfState) {
+        res.status(422).send('Invalid state');
+        return;
+    }
+    CLIENT_KEY = 'awahrdz5tzywmmd2'
+    CLIENT_SECRET = 'b6709fd6a9d6a7abfb08659d0e1c267c'
+    let url_access_token = 'https://open-api.tiktok.com/oauth/access_token/';
+    url_access_token += '?client_key=' + CLIENT_KEY;
+    url_access_token += '&client_secret=' + CLIENT_SECRET;
+    url_access_token += '&code=' + code;
+    url_access_token += '&grant_type=authorization_code';
+
+    fetch(url_access_token, { method: 'post' })
+        .then(res => res.json())
+        .then(json => {
+            res.send(json);
+        });
+})
+
+app.get('/refresh_token/', (req, res) => {
+    const refresh_token = req.query.refresh_token;
+
+    let url_refresh_token = 'https://open-api.tiktok.com/oauth/refresh_token/';
+    url_refresh_token += '?client_key=' + CLIENT_KEY;
+    url_refresh_token += '&grant_type=refresh_token';
+    url_refresh_token += '&refresh_token=' + refresh_token;
+
+    fetch(url_refresh_token, { method: 'post' })
+        .then(res => res.json())
+        .then(json => {
+            res.send(json);
+        });
+})
+
+app.get('/logout', (req, res) => {
+    const { open_id, access_token } = req.query;
+
+    let url_revoke = 'https://open-api.tiktok.com/oauth/revoke/';
+    url_revoke += '?open_id=' + open_id;
+    url_revoke += '&access_token=' + access_token;
+
+    fetch(url_revoke, { method: 'post' })
+        .then(res => res.json())
+        .then(json => {
+            res.send(json);
+        });
+})
